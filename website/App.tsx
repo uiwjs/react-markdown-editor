43688164dbf28236df5c860ef0390a868c5f4b70
jaywcjlove <398188662@qq.com>
2019-04-03 14:40:13 +0800

    Upgrate logo.

--- website/App.tsx
+++ website/App.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import MarkdownEditor from '../src';
 import styles from './App.less';
 import GithubCorner from './components/GithubCorner';
-import logo from './logo.svg';
+import Logo from './Logo';
 
 export default class App extends React.Component {
   public render() {
     return (
       <div className={styles.warpper}>
         <GithubCorner url="https://github.com/uiwjs/react-markdown-editor" />
-        <img src={logo} />
+        <div className={styles.logo}>
+          <Logo />
+        </div>
         <div className={styles.editor}>
           <MarkdownEditor />
         </div>

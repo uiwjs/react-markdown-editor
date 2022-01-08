efd1ef35ae9268bc62deb850ce829458562b2955
jaywcjlove <398188662@qq.com>
2022-01-08 17:12:21 +0800

    chore(deps): update dependency kkt to v7.
    https://github.com/kktjs/kkt/issues/198
    Upgrade react-scripts to v5, Support Webpack 5.x

--- .kktrc.ts
+++ .kktrc.ts
@@ -11,16 +11,48 @@ export default (conf: Configuration, env: 'production' | 'development', options:
   conf = rawModules(conf, env, { ...options });
   conf = scopePluginOptions(conf, env, {
     ...options,
-    allowedFiles: [
-      path.resolve(process.cwd(), 'README.md')
-    ]
+    allowedFiles: [path.resolve(process.cwd(), 'README.md')],
   });
   // Get the project version.
-  conf.plugins!.push(new webpack.DefinePlugin({
-    VERSION: JSON.stringify(pkg.version),
-  }));
+  conf.plugins!.push(
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(pkg.version),
+    }),
+  );
   if (env === 'production') {
-    conf.output = { ...conf.output, publicPath: './' }
+    conf.output = { ...conf.output, publicPath: './' };
+    conf.optimization = {
+      ...conf.optimization,
+      splitChunks: {
+        cacheGroups: {
+          reactvendor: {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+          prismjs: {
+            test: /[\\/]node_modules[\\/](refractor)[\\/]/,
+            name: 'refractor-vendor',
+            chunks: 'all',
+          },
+          uiw: {
+            test: /[\\/]node_modules[\\/](@uiw)[\\/]/,
+            name: 'uiw-vendor',
+            chunks: 'all',
+          },
+          codemirror: {
+            test: /[\\/]node_modules[\\/](codemirror)[\\/]/,
+            name: 'codemirror-vendor',
+            chunks: 'all',
+          },
+          parse5: {
+            test: /[\\/]node_modules[\\/](parse5)[\\/]/,
+            name: 'parse5-vendor',
+            chunks: 'all',
+          },
+        },
+      },
+    };
   }
   return conf;
-}
+};

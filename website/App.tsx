f4355cc981395d049fabaa231fd6c25d2b4d7ff6
jaywcjlove <398188662@qq.com>
2022-08-28 19:29:22 +0800

    website: add backToUp button.

--- website/App.tsx
+++ website/App.tsx
@@ -1,11 +1,12 @@
-import MarkdownEditor from '../';
 import { useState } from 'react';
-import '@wcj/dark-mode';
-import DocumentStr from '../README.md';
-import styles from './App.module.less';
-import Footer from './components/Footer';
+import BackToUp from '@uiw/react-back-to-top';
 import GithubCorner from '@uiw/react-github-corners';
 import MarkdownPreview from '@uiw/react-markdown-preview';
+import '@wcj/dark-mode';
+import DocumentStr from '../README.md';
+import MarkdownEditor from '../';
+import styles from './App.module.less';
+import Footer from './components/Footer';
 import { ReactComponent as Logo } from './logo.svg';
 
 const DocumentStrSource = DocumentStr.replace(/([\s\S]*)<!--dividing-->/, '').replace(/^\n*/g, '');
@@ -17,6 +18,7 @@ export default function App() {
   const [hideToolbar, setHideToolbar] = useState(true);
   return (
     <div className={styles.warpper}>
+      <BackToUp>Top</BackToUp>
       <dark-mode light="Light" dark="Dart" style={{ position: 'fixed' }}></dark-mode>
       <GithubCorner fixed target="__blank" href="https://github.com/uiwjs/react-markdown-editor" />
       <div className={styles.logo}>
